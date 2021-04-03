a19f81e89872aa7e78e77f36456cdb7757412f08
ppruchnicki <pawel.pruchnicki@gambit-it.pl>
2021-04-03 15:39:08 +0200

    Task 2 completed using DRF

--- API/API/views.py
+++ API/API/views.py
@@ -1,17 +1,20 @@
+from .serializers import ProgramySerializer
+from .forms import FTDForm, FTD_ELEMENTYForm
 from django import forms
-from django.http.response import JsonResponse
-from django.views.generic.base import View
-from django.views.generic.edit import DeleteView, UpdateView
-from django.views.generic.list import ListView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from rest_framework.views import APIView
 from django.views.generic import DetailView, CreateView
-from .models import Programy, Osie, Dzialania
+from .models import FTD, Programy, Osie, Dzialania
 from django.db.models import Prefetch
+import json
 
 
-class ProgramyListView(ListView):
+class ProgramyListView(APIView):
     model = Programy
 
-    def get(self, request, *args, **kwargs):
+    """ def get(self, request, *args, **kwargs):
         queryset = Programy.objects.prefetch_related(
             Prefetch('osie', queryset=Osie.objects.prefetch_related('dzialania')))
         programy = []
@@ -25,4 +28,33 @@ class ProgramyListView(ListView):
             programy.append(
                 Programy.toJson(program, osie))
 
-        return JsonResponse(programy, status=200, safe=False)
+        return JsonResponse(programy, status=200, safe=False) """
+
+    def get(self, request):
+        programy = Programy.objects.all()
+        serializer = ProgramySerializer(programy, many=True)
+        return Response(serializer.data)
+
+
+""" class FiltryCreateView(CreateView):
+    model = FTD
+    form_class = FTDForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        # obj.save()
+
+        return JsonResponse(FTD.toJson(obj), status=200, safe=False)
+
+    def post(self, request):
+        if request.POST:
+            form = FTDForm(request.POST)
+        else:
+            body = json.loads(request.body.decode('utf-8'))
+            form = FTDForm(body)
+            if body['struktura']:
+                print(body['struktura'])
+            print(body['nazwa'])
+        if form.is_valid():
+            return self.form_valid(form)
+        return JsonResponse(form.errors, status=400, safe=False) """
